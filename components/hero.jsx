6fff997cb234c1cac63620e538bd1f29332965d3
rijul <rijulgulati16@gmail.com>
2025-04-04 11:49:00 +0530

    new

--- components/hero.jsx
+++ components/hero.jsx
@@ -46,7 +46,7 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-          <Link href="https://youtu.be/gET9d8eP5UA" target="blank">
+          <Link href="https://github.com/Rijul1607/Ascenda" target="blank">
             <Button size="lg" variant="outline" className="px-8">
               Watch Demo
             </Button>
